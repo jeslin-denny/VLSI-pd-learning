c3cb6cda2e78323aa425bdc43c553c5b1408ca24
Jeslin Denny D J <147514067+jeslin-denny@users.noreply.github.com>
2026-01-23 06:31:13 +0530

    Update dff.v

--- rtl/rtl_for_dft/dff.v
+++ rtl/rtl_for_dft/dff.v
@@ -1,1 +1,15 @@
+###############################################
+### Synchronous D Flip flop code in verilog ###
+###############################################
 
+
+module dff (input clk, rst, d, output reg q, output qb);
+  always@(oisedge clk)
+    begin
+    if (rst)
+      q <= 1'b0;
+  else
+    q <= d;
+    end
+  qb = ~q;
+endmodule
